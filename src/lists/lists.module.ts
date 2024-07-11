8ca815c38d9344c148cc31c28936533169bf3e66
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-07-11 20:09:31 +0900

    fix/리스트 모듈에서 엔티티 추가

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ListsService } from './lists.service';
 import { ListsController } from './lists.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { List } from './entities/list.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([List])],
   controllers: [ListsController],
   providers: [ListsService],
 })
