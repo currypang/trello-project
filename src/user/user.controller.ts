e6eba57be366027996e925b6c71cf8b41451e283
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-07-12 11:31:23 +0900

    feat/내정보조회

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Request, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
+  async findUserById(@Request() req) {
+    const userId = req.user.id;
+    const user = await this.userService.findUserById(userId);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: '내 정보 조회에 성공했습니다.',
+      data: user,
+    };
+  }
+}
