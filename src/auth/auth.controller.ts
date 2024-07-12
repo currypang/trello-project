293d463d700b0b1b09e08d43ad0287e93f807eb8
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-07-12 10:27:02 +0900

    fix/엔드포인트 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dtos/sign-up.dto';
 
-@Controller('api/auth')
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
