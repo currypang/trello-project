cb90043d7b14a705ff20b0ee5b0bc19b55461428
minsooC <soochomin@gmail.com>
2024-07-12 10:23:46 +0900

    fix: 보드아이디가 없을때 에러처리추가

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './entities/board.entity';
 import { Repository } from 'typeorm';
@@ -35,6 +35,9 @@ export class BoardService {
             where: {id},
             //relations:{} 추후 카드와 리스트가져오기위해 주석처리함
         })
+        if(!board){
+            throw new NotFoundException('존재하지 않은 보드입니다.')
+        }
         return board
     }
 }
