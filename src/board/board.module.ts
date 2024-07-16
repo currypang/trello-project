8f24d55c67b8eb92904cd000354970070221b589
minsooC <soochomin@gmail.com>
2024-07-16 14:19:39 +0900

    feat/Board_Service_TestCode러프본

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './entities/board.entity';
 import { BoardMembers } from './entities/board-member.entity';
 import { BoardController } from './board.controller';
-import { BoardService } from './board.service';
 import { CardAssigness } from '../cards/entities/card-assigness.entity';
 import { Activity } from '../activity/entities/activity.entity'
 import { InvitationModule } from 'src/invitation/invitation.module';
@@ -12,7 +11,7 @@ import { AuthModule } from 'src/auth/auth.module';
 @Module({
   imports: [TypeOrmModule.forFeature([Board, BoardMembers, CardAssigness, Activity]),InvitationModule,AuthModule],
   controllers: [BoardController],
-  providers: [BoardService],
+  providers: [],
   exports: [],
 })
 export class BoardModule {}
