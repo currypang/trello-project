744886d579ea5a3e0dd0ef2d6b0c573c9e4254b8
romesway <romesway@gmail.com>
2024-07-16 15:12:38 +0900

    fix/email

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CardsService } from './cards.service';
-import { ActivityService } from 'src/activity/activity.service'
+import { ActivityService } from 'src/activity/activity.service';
 import { CardsController } from './cards.controller';
 
 import { Card } from './entities/card.entity';
 import { CardAssigness } from './entities/card-assigness.entity';
-import { BoardMembers } from '../board/entities/board-member.entity'
+import { BoardMembers } from '../board/entities/board-member.entity';
 import { List } from '../lists/entities/list.entity';
-import { Activity } from 'src/activity/entities/activity.entity'
+import { Activity } from 'src/activity/entities/activity.entity';
+import { SseModule } from 'src/sse/sse.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Card, CardAssigness, BoardMembers, List, Activity])],
+  imports: [TypeOrmModule.forFeature([Card, CardAssigness, BoardMembers, List, Activity]), SseModule],
   controllers: [CardsController],
   providers: [CardsService, ActivityService],
 })
